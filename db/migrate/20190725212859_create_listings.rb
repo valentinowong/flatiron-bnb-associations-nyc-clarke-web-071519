3217baeaccda1133fd85e76d5dff6a21d881b67b
Valentino Wong <valentino.wong@gmail.com>
2019-07-25 17:59:21 -0400

    Done.

---
+++ db/migrate/20190725212859_create_listings.rb
@@ -0,0 +1,14 @@
+class CreateListings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :listings do |t|
+      t.string :title
+      t.string :address
+      t.string :listing_type
+      t.text :description
+      t.float :price
+      t.integer :neighborhood_id
+      t.integer :host_id
+      t.timestamps
+    end
+  end
+end
